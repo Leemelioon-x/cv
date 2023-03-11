a8faff75e4bba795f0c5683671a3bcdc9b1f5fd6
Leemelioon <kotxbossx@gmail.com>
2023-03-11 14:07:32 +0300

    commit - add new style in Description.tsx

--- src/components/descriptionPage/Description.tsx
+++ src/components/descriptionPage/Description.tsx
@@ -7,14 +7,50 @@ export const Description = () => {
             <div className={s.programmerName}>
                 Мартынчук Кирилл Дмитриевич
                 <div className={s.specializationName}>
-                    frontend developer(React + TS)
+                    Frontend developer(React + TS)
                 </div>
             </div>
-            <div>
+            <div className={s.descriptionAuthor}>
                 Sobre
-                <div>
+                <div className={s.aboutAuthor}>
                     Creative student-interface designer FrontEnd developer, who took courses from it-incubator.
-                    I am currently developing and participating in projects and tasks focused on a web designer in order to improve my skills.
+                    I am currently developing and participating in projects and tasks focused on a web designer in order
+                    to improve my skills.
+                </div>
+            </div>
+            <div className={s.technologyStackContainer}>
+                Technology stack
+                <div className={s.technologyStack}>
+                    <div className={s.technology}>
+                        <img src="/src/assets/icon/maxresdefault.jpg" alt="HTML"/>
+                        <div className={s.technologyName}>
+                            HTLM5+CSS3
+                        </div>
+                    </div>
+                    <div>
+                        <img src="src/assets/icon/jetbrains-webstorm2487.jpg" alt="Webstorm"/>
+                        <div className={s.technologyName}>
+                            Webstorm
+                        </div>
+                    </div>
+                    <div>
+                        <img src="src/assets/icon/unnamed.jpg" alt="GitHub"/>
+                        <div className={s.technologyName}>
+                            GitHub
+                        </div>
+                    </div>
+                    <div>
+                        <img src="src/assets/icon/FcsyONIXkAAYsJO.jpg" alt="GitHub"/>
+                        <div className={s.technologyName}>
+                            Figma
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={s.workExperienceContainer}>
+                Work experience
+                <div className={s.workExperienceDescription}>
+
                 </div>
             </div>
         </div>
